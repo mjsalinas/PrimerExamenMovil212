5d13cd4e4849e99c0b5f0e97911bba875e60a614
Jennsen Murillo <Jennsenasdrubal@gmail.com>
2025-11-14 23:06:03 -0600

    ultimo commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,24 @@
+// App.tsx
 import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
-import ProfileScreen from "./screens/ProfileScreen";
-import SettingsScreen from "./screens/SettingsScreen";
-const Stack = createNativeStackNavigator();
+import TabsNavigator from "./navigation/TabsNavigator";
 
+export type RootStackParamList = {
+  Home: undefined;
+  Tabs: { screen?: string };
+};
 
-  export default function App() {
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function App() {
   return (
     <NavigationContainer>
-    <Stack.Navigator initialRouteName='Home'>
-      <Stack.Screen name='Home' component={HomeScreen} />
-      <Stack.Screen name='Profile' component={SettingsScreen} />
-       <Stack.Screen name='Settings' component={SettingsScreen} />
-    </Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Tabs" component={TabsNavigator} />
+      </Stack.Navigator>
     </NavigationContainer>
-
   );
 }
